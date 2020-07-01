5cc32b73ac1fbf16371053c4e5cae3a0defe1d91
Emkis <nicolasemkis@gmail.com>
2020-07-01 02:37:43 -0300

    add component

---
+++ Thinking small - Creating reusable components with Vue/code/src/components/ButtonFollow.vue
@@ -0,0 +1,34 @@
+<template>
+  <button class="button-follow">
+    <IconPlus size="18" />
+    Follow
+  </button>
+</template>
+
+<script>
+import { IconPlus } from '@/components/icons'
+
+export default {
+  name: 'ButtonFollow',
+  components: { IconPlus },
+}
+</script>
+
+<style lang="scss" scoped>
+.button-follow {
+  display: flex !important;
+  align-items: center;
+  padding: rem(10px 20px);
+  cursor: pointer;
+
+  border: 0;
+  border-radius: rem(4px);
+  background: var(--color-primary);
+  color: white;
+  vertical-align: middle;
+
+  > svg {
+    margin-right: rem(8px);
+  }
+}
+</style>

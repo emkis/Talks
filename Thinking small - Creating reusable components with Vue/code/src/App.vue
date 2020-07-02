f63068e50c30dd7fe6ab52f923f88ceb08421865
Emkis <nicolasemkis@gmail.com>
2020-07-02 09:13:26 -0300

    delete unnecessary mixin

--- Thinking small - Creating reusable components with Vue/code/src/App.vue
+++ Thinking small - Creating reusable components with Vue/code/src/App.vue
@@ -6,11 +6,9 @@
 </template>
 
 <script>
-import fullViewportHeightMixin from '@/mixins/fullViewportHeightMixin'
 import ContextMenuRoot from '@/components/ContextMenuRoot'
 
 export default {
-  mixins: [fullViewportHeightMixin],
   components: { ContextMenuRoot },
 }
 </script>

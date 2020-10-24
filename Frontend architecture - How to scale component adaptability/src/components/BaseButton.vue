9f0a233c85a8767eff05cd335b06d02d957d2438
emkis <nicolasemkis@gmail.com>
2020-10-24 16:16:13 -0300

    feat: add button components

---
+++ Frontend architecture - How to scale component adaptability/src/components/BaseButton.vue
@@ -0,0 +1,33 @@
+<template>
+  <button class="BaseButton" v-on="$listeners">
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'BaseButton',
+}
+</script>
+
+<style lang="scss" scoped>
+.BaseButton {
+  border: 0;
+  height: rem(40px);
+  padding: rem(0 30px);
+
+  font-family: $font-title;
+  font-weight: 700;
+
+  color: var(--c-white-lilac);
+  border-radius: $radius-button;
+  background: var(--c-royal-blue);
+
+  white-space: nowrap;
+  cursor: pointer;
+
+  &:hover {
+    opacity: 0.9;
+  }
+}
+</style>
---
+++ Frontend architecture - How to scale component adaptability/src/components/ButtonCircle.vue
@@ -0,0 +1,38 @@
+<template>
+  <button class="ButtonCircle" v-on="$listeners">
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'ButtonCircle',
+}
+</script>
+
+<style lang="scss" scoped>
+.ButtonCircle {
+  $button-size: rem(40px);
+
+  width: $button-size;
+  height: $button-size;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  border: 0;
+  border-radius: 50%;
+  padding: rem(10px);
+
+  color: var(--c-tuna);
+  background: var(--c-link-water);
+
+  transition: transform ease 200ms;
+  cursor: pointer;
+
+  &:hover {
+    transform: scale(1.16);
+  }
+}
+</style>

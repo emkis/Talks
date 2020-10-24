5c26cebf7ce36346a217abb46a39b97bee347e53
emkis <nicolasemkis@gmail.com>
2020-10-24 00:06:48 -0300

    style: add prettier configurations & lint files

--- Frontend architecture - How to scale component adaptability/.eslintrc.js
+++ Frontend architecture - How to scale component adaptability/.eslintrc.js
@@ -3,12 +3,14 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/prettier'],
   parserOptions: {
-    parser: "babel-eslint",
+    parser: 'babel-eslint',
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    semi: ['error', 'never'],
+    'prettier/prettier': 'error',
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
-};
+}

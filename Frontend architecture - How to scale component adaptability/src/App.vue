5cea06ea6c02fbc66347b01b3ae932c71be7a85e
emkis <nicolasemkis@gmail.com>
2020-10-24 01:24:39 -0300

    feat: add app global styles setup

--- Frontend architecture - How to scale component adaptability/src/App.vue
+++ Frontend architecture - How to scale component adaptability/src/App.vue
@@ -18,11 +18,10 @@ export default {
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  img {
+    display: inline-block;
+  }
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding-top: 60px;
 }
 </style>

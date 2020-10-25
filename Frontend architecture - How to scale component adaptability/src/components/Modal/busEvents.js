fc55b5ecebd23e7210ba4805397debb48f24fbb1
emkis <nicolasemkis@gmail.com>
2020-10-25 02:57:45 -0300

    fix: fixes modal options name

--- Frontend architecture - How to scale component adaptability/src/components/Modal/busEvents.js
+++ Frontend architecture - How to scale component adaptability/src/components/Modal/busEvents.js
@@ -3,8 +3,8 @@ import { EventBus } from '@/eventBus'
 export const OPEN = '@modal/OPEN'
 export const CLOSE = '@modal/CLOSE'
 
-export function openModal({ component, componentProps, isClosable = true }) {
-  EventBus.$emit(OPEN, { component, componentProps, isClosable })
+export function openModal({ component, props, isClosable = true }) {
+  EventBus.$emit(OPEN, { component, props, isClosable })
 }
 
 export function closeModal() {

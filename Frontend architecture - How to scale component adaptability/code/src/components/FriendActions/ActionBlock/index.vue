ca0f2b71b21bb104dcabcdb12cd2d19f3aa4520b
emkis <nicolasemkis@gmail.com>
2020-11-12 01:54:26 -0300

    feat: add crazy component do friend action

--- Frontend architecture - How to scale component adaptability/code/src/components/FriendActions/ActionBlock/index.vue
+++ Frontend architecture - How to scale component adaptability/code/src/components/FriendActions/ActionBlock/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <HeaderContainer :type="type" />
+    <CrazyAnimation />
     <BodyContainer :type="type" />
     <FooterContainer :type="type" :handler="handler" />
   </div>
@@ -12,10 +13,16 @@ import defaultProps from '../properties/defaultProperties'
 import HeaderContainer from '../Headers/HeaderContainer'
 import BodyContainer from '../Bodys/BodyContainer'
 import FooterContainer from '../Footers/FooterContainer'
+import CrazyAnimation from '@/components/CrazyAnimation'
 
 export default {
   name: 'ActionBlock',
   props: { ...defaultProps },
-  components: { BodyContainer, HeaderContainer, FooterContainer },
+  components: {
+    BodyContainer,
+    HeaderContainer,
+    FooterContainer,
+    CrazyAnimation,
+  },
 }
 </script>

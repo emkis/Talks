b9e100432828d1b26f00495571941ef02a07cff2
emkis <nicolasemkis@gmail.com>
2020-11-12 01:52:38 -0300

    feat: add Crazy component

---
+++ Frontend architecture - How to scale component adaptability/code/src/components/CrazyAnimation.vue
@@ -0,0 +1,20 @@
+<template>
+  <img
+    class="CrazyAnimation"
+    src="https://media.giphy.com/media/Lq1YCqwsFjCe6DkPLf/source.gif"
+  />
+
+  <!-- Illustration by: https://giphy.com/gifs/Lq1YCqwsFjCe6DkPLf -->
+</template>
+
+<script>
+export default { name: 'CrazyAnimation' }
+</script>
+
+<style lang="scss" scoped>
+.CrazyAnimation {
+  width: 100%;
+  height: 180px;
+  object-fit: cover;
+}
+</style>

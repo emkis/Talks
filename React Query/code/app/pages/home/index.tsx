56a73c716b1b287c367ff2a90262c3409f50498d
emkis <nicolasemkis@gmail.com>
2023-02-05 11:58:00 +0000

    rename src to app

--- React Query/code/app/pages/home/index.tsx
+++ React Query/code/app/pages/home/index.tsx
@@ -2,7 +2,7 @@ import { Sidebar } from '@components/Sidebar'
 
 export function Home() {
   return (
-    <div className="grid grid-cols-[333px_1fr]">
+    <div className="grid grid-cols-[300px_1fr]">
       <Sidebar></Sidebar>
 
       <main className="p-6">

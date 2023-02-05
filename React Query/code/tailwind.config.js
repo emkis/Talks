414249b994d8191e7ab2b934cd72429a574abebb
emkis <nicolasemkis@gmail.com>
2023-02-05 13:41:05 +0000

    rename src to app

--- React Query/code/tailwind.config.js
+++ React Query/code/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ['./index.html', './src/**/*.{ts,tsx}'],
+  content: ['./index.html', './app/**/*.{ts,tsx}'],
   theme: {
     extend: {},
   },

5f0b6dce1ac2e5a44c99aa3886835952f4eb7b59
emkis <nicolasemkis@gmail.com>
2023-02-05 20:20:10 +0000

    add permissions page

--- React Query/code/app/pages/permissions/index.tsx
+++ React Query/code/app/pages/permissions/index.tsx
@@ -1,3 +1,27 @@
+import { usePermissionsQuery } from './queries/permissions'
+
 export function Permissions() {
-  return <h1 className="text-4xl font-bold text-black">Permissions page</h1>
+  const permissionsQuery = usePermissionsQuery()
+
+  return (
+    <section>
+      <h1 className="text-4xl font-bold text-black">
+        Permissions
+        <small className="text-sm">{permissionsQuery.isFetching ? '(updating...)' : null}</small>
+      </h1>
+
+      <ul className="grid gap-2 py-8">
+        {permissionsQuery.data?.map((permission, permissionIndex) => (
+          <li
+            key={permissionIndex}
+            className="max-w-md rounded-xl border-2 border-gray-400 p-3 font-mono text-lg font-bold text-gray-800"
+          >
+            {permission.startsWith('create') && <span>🟢</span>}
+            {permission.startsWith('read') && <span>🔵</span>}
+            {permission.startsWith('update') && <span>⚫️</span>} {permission}
+          </li>
+        ))}
+      </ul>
+    </section>
+  )
 }

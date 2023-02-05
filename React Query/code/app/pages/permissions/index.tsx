fb80f818760d8fc589db70431a6a83b14aa7c038
emkis <nicolasemkis@gmail.com>
2023-02-05 21:49:12 +0000

    only show add button when granted

--- React Query/code/app/pages/permissions/index.tsx
+++ React Query/code/app/pages/permissions/index.tsx
@@ -1,14 +1,19 @@
-import { usePermissionsQuery } from './queries/permissions'
+import { AddButton } from '@shared/components/AddButton'
+import { usePermissions, usePermissionsQuery } from '@shared/authorization'
 
 export function Permissions() {
+  const isPermissionsGranted = usePermissions(['create:permissions'])
   const permissionsQuery = usePermissionsQuery()
 
   return (
     <section>
-      <h1 className="text-4xl font-bold text-black">
-        Permissions
-        <small className="text-sm">{permissionsQuery.isFetching ? '(updating...)' : null}</small>
-      </h1>
+      <div className="flex justify-between">
+        <h1 className="text-4xl font-bold text-black">
+          Permissions
+          <small className="text-sm">{permissionsQuery.isFetching ? '(updating...)' : null}</small>
+        </h1>
+        {isPermissionsGranted && <AddButton className="text-slate-800" />}
+      </div>
 
       <ul className="grid gap-2 py-8">
         {permissionsQuery.data?.map((permission, permissionIndex) => (

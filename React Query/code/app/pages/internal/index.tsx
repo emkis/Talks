3e4717603def65460f65a211789fcb2cf39086b4
emkis <nicolasemkis@gmail.com>
2023-02-05 20:13:38 +0000

    add internal permissions for demo

--- React Query/code/app/pages/internal/index.tsx
+++ React Query/code/app/pages/internal/index.tsx
@@ -1,3 +1,31 @@
+import { environment } from '@shared/utilities/environment'
+
+function sendOperation(operation: string) {
+  fetch(`${environment.internalApiUrl}/permissions-internal`, {
+    method: 'POST',
+    body: JSON.stringify({ operation }),
+  })
+}
+
 export function Internal() {
-  return <h1 className="text-4xl font-bold text-black">Internal page</h1>
+  const operations = ['reset', 'default', 'readonly', 'add-p', 'remove-p']
+
+  return (
+    <section className="grid gap-8">
+      <h1 className="text-4xl font-bold text-black">Internal</h1>
+
+      {operations.map((operation) => {
+        return (
+          <button
+            key={operation}
+            className="flex h-12 w-full items-center justify-center rounded-lg bg-slate-900 px-6 font-semibold text-white hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 sm:w-auto"
+            type="button"
+            onClick={() => sendOperation(operation)}
+          >
+            {operation} permissions
+          </button>
+        )
+      })}
+    </section>
+  )
 }
---
+++ React Query/code/serverless/permissions-internal/index.ts
@@ -0,0 +1,69 @@
+import type { Handler, HandlerEvent } from '@netlify/functions'
+import { supabase } from '../shared/client'
+import { defaultHeaders } from '../shared/utilities/headers'
+
+const permissions = {
+  create: ['create:projects', 'create:user', 'create:permissions'] as const,
+  read: ['read:projects', 'read:user', 'read:permissions'] as const,
+  update: ['update:projects', 'update:user'] as const,
+  all: () => [...permissions.create, ...permissions.read, ...permissions.update],
+}
+
+function insertPermission(permission: string) {
+  return supabase.from('permissions').insert({ name: permission })
+}
+
+async function resetAllPermissions() {
+  await Promise.all(
+    permissions.all().map((permission) => {
+      return supabase.from('permissions').delete().eq('name', permission)
+    })
+  )
+  return { allGood: true }
+}
+
+async function defaultPermissions() {
+  await Promise.all(permissions.all().sort().map(insertPermission))
+  return { allGood: true }
+}
+
+async function readPermissionsOnly() {
+  await Promise.all(permissions.read.map(insertPermission))
+  return { allGood: true }
+}
+
+function addPermissions() {
+  return supabase
+    .from('permissions')
+    .insert([{ name: 'read:permissions' }, { name: 'create:permissions' }])
+}
+
+async function removePermissions() {
+  await supabase.from('permissions').delete().eq('name', 'read:permissions')
+  await supabase.from('permissions').delete().eq('name', 'create:permissions')
+  return { allGood: true }
+}
+
+const handler: Handler = async (event: HandlerEvent) => {
+  const body = JSON.parse(event.body!)
+
+  // @ts-expect-error I don't have time to understand this error, I need to finish this demo
+  const operations = new Map([
+    ['reset', resetAllPermissions],
+    ['default', defaultPermissions],
+    ['readonly', readPermissionsOnly],
+    ['add-p', addPermissions],
+    ['remove-p', removePermissions],
+  ])
+
+  const operation = operations.get(body.operation)
+  const response = await operation!()
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(response),
+    headers: defaultHeaders(),
+  }
+}
+
+export { handler }

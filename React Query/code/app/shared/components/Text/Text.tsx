99e756d9a410d3145a64b87fcd1921ae7d01e830
emkis <nicolasemkis@gmail.com>
2023-02-07 23:12:12 +0000

    add text component

---
+++ React Query/code/app/shared/components/Text/Text.tsx
@@ -0,0 +1,10 @@
+import type { WithChildren } from '@shared/types/react'
+import { cn } from '@shared/utilities/cn'
+
+type TextProps = WithChildren<{
+  className?: string
+}>
+
+export function Text({ className, children }: TextProps) {
+  return <p className={cn('text-2xl leading-normal text-gray-900', className)}>{children}</p>
+}

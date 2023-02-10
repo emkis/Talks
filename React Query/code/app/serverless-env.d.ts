3d98d2db6c90a978d657a318ff305ca911b364c6
emkis <nicolasemkis@gmail.com>
2023-02-10 16:45:38 +0000

    typo

--- React Query/code/app/serverless-env.d.ts
+++ React Query/code/app/serverless-env.d.ts
@@ -7,7 +7,7 @@ declare global {
   }
 }
 
-// For this overwrite to work, it's required to export from this module
+// For this global types to work, it's required to export from this module
 // otherwise the type inference doesn't work!
 // Read more about it here: https://typescript.tv/errors/#TS2669
 export {}

36e240b7418bdc89050197158163a3ff20b4a1a8
emkis <nicolasemkis@gmail.com>
2023-02-11 00:39:38 +0000

    change default headers

--- React Query/code/serverless/shared/utilities/headers.ts
+++ React Query/code/serverless/shared/utilities/headers.ts
@@ -2,6 +2,6 @@ export function defaultHeaders() {
   return {
     'Content-Type': 'application/json',
     'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
+    Vary: 'Origin',
   }
 }
